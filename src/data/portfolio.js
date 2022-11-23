ee6f42ba0dff30ad3dab4bfd70aaf61d7f3241a9
Greenreggie10 <Reggie.green10@yahoo.com>
2022-11-22 19:48:10 -0500

    portfolio updat

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,32 +1,27 @@
 export const portfolioData = [
         {
-            "cover": require("../assets/images/logo1.png"),
+            "cover": require("../assets/images/home-screen out fox.png"),
             "title": "Outfoxed",
             "description": "JQuery,HTML5,JavaScript,CSS",
             "url": "https://bpiper91.github.io/outfoxed/"
         },
         {
-            "cover": require("../assets/images/logo1.png"),
+            "cover": require("../assets/images/JATE.png"),
             "title": "J.A.T.E",
             "description": "Webpack, Express.js, Node.js ",
             "url": "https://peaceful-caverns-83980.herokuapp.com/"
         },
         {
-            "cover": require("../assets/images/logo1.png"),
+            "cover": require("../assets/images/(e-commerce).png"),
             "title": "E-commerce",
             "description": " MySQL, Express.js, Node.js",
             "url": "https://drive.google.com/file/d/1tgRm2GEImJgwwY6AAQOBJkiPG5O_NRTL/view"
         },
         {
-            "cover": require("../assets/images/logo1.png"),
+            "cover": require("../assets/images/(movie gallery).png"),
             "title": "Movie-gallary",
             "description": " RESTful API, MySQL, Seq ORM",
             "url": "https://movie-galleries.herokuapp.com/"
         },
-        {
-            "cover": require("../assets/images/logo1.png"),
-            "title": "Movie-gallary",
-            "description": " RESTful API, MySQL, Seq ORM",
-            "url": "https://movie-galleries.herokuapp.com/"
-        }
+       
     ]
